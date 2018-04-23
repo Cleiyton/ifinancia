fabe0514e5ee523c31f88efb9a727668d636984f
cleiton Alex <cleiyton2020@gmail.com>
2018-04-23 16:41:04 -0300

    foi modificado o index e criei uma pagina login so rodar a aplicação e chamar depois do 8080/login que esta funcionando e ver como ficou

--- src/main/java/com/ifinancia/ifinancia/IdenxController.java
+++ src/main/java/com/ifinancia/ifinancia/IdenxController.java
@@ -1,13 +1,19 @@
 package com.ifinancia.ifinancia;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 public class IdenxController {
 	
 	@RequestMapping("/")
 	public String index() {
 		
-		return "index.html";
+		return "index";
 	}
 
+	@RequestMapping("/login")
+	public String login() {
+		return "financias/login";
+	}
 }
